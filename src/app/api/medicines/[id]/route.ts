4b672b87e28d73c188559e8301d93f7934b160ee
tharukab95 <acslashed@yahoo.com>
2024-12-11 09:37:15 +0530

    fix: type issue fix new

--- src/app/api/medicines/[id]/route.ts
+++ src/app/api/medicines/[id]/route.ts
@@ -1,18 +1,18 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { connectDB } from "@/lib/mongodb";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { MedicineModel } from "@/models";
 import { toClientModel } from "@/lib/utils";
 
 // GET single medicine
 export async function GET(
-  req: Request,
-  context: { params: { id: string } }
+  request: NextRequest,
+  { params }: { params: { id: string } }
 ) {
   try {
     await connectDB();
-    const medicine = await MedicineModel.findById(context.params.id).lean();
+    const medicine = await MedicineModel.findById(params.id).lean();
     if (!medicine) {
       return NextResponse.json(
         { message: "Medicine not found" },
@@ -30,14 +30,14 @@ export async function GET(
 
 // PUT update medicine
 export async function PUT(
-  req: Request,
-  context: { params: { id: string } }
+  request: NextRequest,
+  { params }: { params: { id: string } }
 ) {
   try {
     await connectDB();
-    const body = await req.json();
+    const body = await request.json();
     const medicine = await MedicineModel.findByIdAndUpdate(
-      context.params.id,
+      params.id,
       { $set: body },
       { new: true }
     ).lean();
@@ -60,12 +60,12 @@ export async function PUT(
 
 // DELETE medicine
 export async function DELETE(
-  req: Request,
-  context: { params: { id: string } }
+  request: NextRequest,
+  { params }: { params: { id: string } }
 ) {
   try {
     await connectDB();
-    const medicine = await MedicineModel.findByIdAndDelete(context.params.id);
+    const medicine = await MedicineModel.findByIdAndDelete(params.id);
 
     if (!medicine) {
       return NextResponse.json(
